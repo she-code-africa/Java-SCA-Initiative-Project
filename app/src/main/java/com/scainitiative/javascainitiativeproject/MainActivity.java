752d57b276499e6627151a67759cea6376c573fa
Yczar <babalolagbogo@gmail.com>
2020-12-16 02:17:21 +0100

    FLAG_LAYOUT_NO_LIMITS

--- app/src/main/java/com/scainitiative/javascainitiativeproject/MainActivity.java
+++ app/src/main/java/com/scainitiative/javascainitiativeproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.scainitiative.javascainitiativeproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
